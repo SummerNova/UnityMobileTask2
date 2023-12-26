f1fe83c7ace143bc2251b4e394dd946d36f0502e
SummerNova <137454229+SummerNova@users.noreply.github.com>
2023-12-26 13:46:43 +0200

    joystick works, for the most part

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,16 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Vector2 MoveDirection;
-
-    void Start()
-    {
-        
-    }
+    public Vector2 MoveDirection = Vector2.zero;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(MoveDirection*Time.deltaTime);
     }
 }
